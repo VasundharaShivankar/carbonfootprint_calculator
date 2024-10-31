60db34ba9769ddbaf93381a4d004415432ddb827
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-10-31 22:19:52 +0530

    navbar edit

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -38,9 +38,10 @@ export default function Navbar() {
 
   return (
     <>
+    
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark py-3" >
         <div className="container-fluid navsize">
-          <Link className="navbar-brand" to="/Shopping">Shopping</Link>
+          <Link className="navbar-brand" to="/Shopping">Calculator</Link>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -51,7 +52,7 @@ export default function Navbar() {
                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/review">Reviews</Link>
+                <Link className="nav-link active" aria-current="page" to="/review">Community Forum</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/contact">Contact Us</Link>
